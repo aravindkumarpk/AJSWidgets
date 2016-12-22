fb8de69dbc7f9471eaefdceeb886bb51c828204b
aravindkumar <aravindkumar.p.k@gmail.com>
2016-12-22 13:07:01 -0500

    Updated
    Updated

--- app/components/directives/field.directive.js
+++ app/components/directives/field.directive.js
@@ -1,50 +1,49 @@
 'use strict';
+ 
+var __indexOf = [].indexOf || function(item) {
+        for (var i = 0, l = this.length; i < l; i++) {
+            if (i in this && this[i] === item) return i;
+        }
+        return -1;
+    };
 
-// coffeescript's for in loop
-//var __indexOf = [].indexOf || function(item) {
-//        for (var i = 0, l = this.length; i < l; i++) {
-//            if (i in this && this[i] === item) return i;
-//        }
-//        return -1;
-//    };
+angular.module('SFSiteVisitApp').directive('fieldsDirective', function () {
 
-//angular.module('SFSiteVisitApp').directive('fieldsDirective', function () {
+    var getTemplateUrl = function (question) {
+         
+        var type = 'TEXT';
+        var templateUrl = './Views/FormBuilder/directive-templates/field/';
+        var supported_fields = [
+            'textfield',
+            'email',
+            'TEXT',
+            'CHECK',
+            'DATE',
+            'COMBO',
+            'hidden',
+            'password',
+            'OPTION'
+        ]
 
-//    //var getTemplateUrl = function (question) {
-//    //    //alert(field.obj_type);
-//    //    var type = 'TEXT';
-//    //    var templateUrl = './Views/FormBuilder/directive-templates/field/';
-//    //    var supported_fields = [
-//    //        //'textfield',
-//    //        //'email',
-//    //        'TEXT',
-//    //        'CHECK',
-//    //        'DATE',
-//    //        'COMBO',
-//    //        //'hidden',
-//    //        //'password',
-//    //        'OPTION'
-//    //    ]
+        if (__indexOf.call(supported_fields, type) >= 0) {
+            return templateUrl += type + '.html';
+        }
 
-//    //    if (__indexOf.call(supported_fields, type) >= 0) {
-//    //        return templateUrl += type + '.html';
-//    //    }
+        //alert(templateUrl);
+    }
 
-//    //    //alert(templateUrl);
-//    //}
+    var linker = function (scope, element) {
+        // GET template content from path
+        //var templateUrl = getTemplateUrl(scope.question);
+        var templateUrl = './Views/FormBuilder/directive-templates/field/TEXT.html';
+        $http.get(templateUrl).success(function (data) {
+            element.html(data);
+            $compile(element.contents())(scope);
+        });
+    }
 
-//    //var linker = function (scope, element) {
-//    //    // GET template content from path
-//    //    //var templateUrl = getTemplateUrl(scope.question);
-//    //    var templateUrl = './Views/FormBuilder/directive-templates/field/TEXT.html';
-//    //    $http.get(templateUrl).success(function (data) {
-//    //        element.html(data);
-//    //        $compile(element.contents())(scope);
-//    //    });
-//    //}
-
-//        return {
-//            templateUrl: './Views/FormBuilder/directive-templates/field/TEXT.html',
-//            restrict: 'E'        
-//        };
-//});
+        return {
+            templateUrl: './Views/FormBuilder/directive-templates/field/TEXT.html',
+            restrict: 'E'        
+        };
+});
--- app/components/directives/form.directive.js
+++ app/components/directives/form.directive.js
@@ -1,22 +1,22 @@
 'use strict';
 
-//angular.module('SFSiteVisitApp').directive('formsDirective', function () {
-//    return {
-//        controller: function ($scope) {
-//            $scope.submit = function () {
-//                alert('Form submitted..');
-//                $scope.forms.submitted = true;
-//            }
+angular.module('AJSWidgets').directive('formsDirective', function () {
+    return {
+        controller: function ($scope) {
+            $scope.submit = function () {
+                alert('Form submitted..');
+                $scope.forms.submitted = true;
+            }
 
-//            $scope.cancel = function () {
-//                alert('Form canceled..');
-//            }
-//        },
-//        templateUrl: function () { return './Views/FormBuilder/directive-templates/form/form.html' },
-//        restrict: 'E'
-//        //,
-//        //scope: {
-//        //        forms: '='
-//        //}
-//    };
-//});
+            $scope.cancel = function () {
+                alert('Form canceled..');
+            }
+        },
+        templateUrl: function () { return './Views/FormBuilder/directive-templates/form/form.html' },
+        restrict: 'E'
+        ,
+        scope: {
+                forms: '='
+        }
+    };
+});
