8924d206cdb1f1f199da6277c5d67b59c5e3f87d
Aravind Kumar <aravindkumar.p.k@gmail.com>
2016-12-22 16:01:53 -0500

    Update form.directive.js

--- app/components/directives/form.directive.js
+++ app/components/directives/form.directive.js
@@ -4,12 +4,12 @@ angular.module('AJSWidgets').directive('formsDirective', function () {
     return {
         controller: function ($scope) {
             $scope.submit = function () {
-                alert('Form submitted..');
+                //alert('Form submitted..');
                 $scope.forms.submitted = true;
             }
 
             $scope.cancel = function () {
-                alert('Form canceled..');
+                //alert('Form canceled..');
             }
         },
         templateUrl: function () { return './Views/FormBuilder/directive-templates/form/form.html' },
